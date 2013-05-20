a9395b8b3b3b61f5ea37c948df45124aaaa9e846
Dan Fox <iamdanfox@gmail.com>
2013-05-20 16:39:02 +0100

    Sizing TODO

--- src/controller/DiamondTargetField.java
+++ src/controller/DiamondTargetField.java
@@ -1,5 +1,6 @@
 package controller;
 
+//import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
@@ -20,7 +21,7 @@ public class DiamondTargetField extends JTextField implements CaveListener, Acti
 	public DiamondTargetField(Cave cave, InteractionMode mode){
 		this.cave = cave;
 		this.setText(""+cave.getDiamondTarget());
-		
+		//this.setMaximumSize(new Dimension(50,200)); // TODO limit size of this field when using a large cave.
 		// make cave react to field
 		this.addActionListener(this);
 		this.addFocusListener(this);
